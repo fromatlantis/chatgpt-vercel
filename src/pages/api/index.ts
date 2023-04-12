5ca2d9a677786aa4e13b3db90a65847e39dd99ef
VincentNeo <76011299@qq.com>
2023-04-12 17:41:38 +0800

    Update index.ts

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -14,24 +14,7 @@ export const config = {
    * only for vercel
    */
   regions: [
-    "arn1",
-    "bom1",
-    "bru1",
-    "cdg1",
-    "cle1",
-    "cpt1",
-    "dub1",
-    "fra1",
-    "gru1",
-    "hnd1",
-    "iad1",
-    "icn1",
-    "kix1",
-    "lhr1",
-    "pdx1",
-    "sfo1",
-    "sin1",
-    "syd1"
+    "iad1"
   ]
 }
 
